919c0d8b5d69a349df51a3a3f577b37a5bf5580a
Rakib <xeon.rakibul@gmail.com>
2025-07-15 22:41:12 +0600

    initial setup

---
+++ src/app/pages/shared/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <>
+          <p>footer</p>  
+        </>
+    );
+};
+
+export default footer;
---
+++ src/app/pages/shared/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navbar = () => {
+    return (
+        <>
+         <p>navbar</p>   
+        </>
+    );
+};
+
+export default navbar;
