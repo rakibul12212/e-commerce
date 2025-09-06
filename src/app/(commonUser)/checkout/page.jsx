d218f934f9d8ba93fe08f60ce9d088dddbc3317a
rakib <rakibulislamr83@gmail.com>
2025-09-06 18:10:19 +0600

    redirect

--- src/app/(commonUser)/checkout/page.jsx
+++ src/app/(commonUser)/checkout/page.jsx
@@ -3,11 +3,13 @@ import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 import CheckoutPage from "@/components/pages/home/container/CheckoutPage/CheckoutPage";
 
-
 export default async function Checkout() {
   const session = await getServerSession(authOptions);
 
-  if (!session) redirect("/signin"); 
+  if (!session) {
+    const params = new URLSearchParams({ callbackUrl: "/checkout" });
+    redirect(`/signin?${params.toString()}`);
+  }
 
-  return <CheckoutPage />; 
+  return <CheckoutPage />;
 }
--- src/components/pages/register/signin/signin.jsx
+++ src/components/pages/register/signin/signin.jsx
@@ -1,18 +1,24 @@
 "use client";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export default function SigninPage() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") 
+
   return (
     <div className="min-h-[60vh] flex flex-col items-center justify-center gap-3">
       <h1 className="text-2xl font-semibold">Sign in</h1>
+
       <button
-        onClick={() => signIn("google")}
+        onClick={() => signIn("google", { callbackUrl })}
         className="px-4 py-2 rounded bg-blue-600 text-white"
       >
         Continue with Google
       </button>
+
       <button
-        onClick={() => signIn("github")}
+        onClick={() => signIn("github", { callbackUrl })}
         className="px-4 py-2 rounded bg-neutral-800 text-white"
       >
         Continue with GitHub
